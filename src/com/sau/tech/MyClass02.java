db17965a0cc9a9ebd98bd45ac87fd093f553fe43
Aztr0boy <ntx.duos0069@gmail.com>
2025-01-14 17:00:03 +0700

    Default JAVA Project -V.4 : Edit Mc 3

--- src/com/sau/tech/MyClass02.java
+++ src/com/sau/tech/MyClass02.java
@@ -1,4 +1,29 @@
 package com.sau.tech;
 
 public class MyClass02 {
+    public static void main(String[] args) {
+        System.out.println("ZXC");
+        System.out.print("XCZ");
+        System.out.println("ZZZ");
+
+
+
+        System.out.println('M');
+
+        System.out.println(123);
+        System.out.println(1324);
+        System.out.println(456);
+        System.out.println(4567);
+
+
+        System.out.println(true);
+        System.out.println(false);
+
+        //Expression นิพจน์
+        System.out.println(1+3);
+
+        //ใฃ้เครื่องหมาย + ในการประสาน/เชื่อมข้อมูลในเเต่ละประเภทเข้าด้วยกัน
+        System.out.println(10 + 10 + "10" + 30);
+        System.out.println("10"+ 10 + 10 + 30);
+    }
 }
